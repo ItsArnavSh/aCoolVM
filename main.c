07da4f08486e08a67c94618dd393b742912483c4
ItsArnavSh <its.arnav.sh@gmail.com>
2024-12-14 00:10:43 +0530

    File handling

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "bootup.h"
 #include "bootup.h"
 //Now we will
 
 int main(int argc, char** argv){
     bootupVM();
     //Now we will load the asm instruction from the file
+    if(argc!=2){
+        printf("Incorrect Code");
+        exit(0);
+    }
+    loadIntoMemory(asmParser(argv));
 }
